d39485b8bc9ab31e7e4e468bbf99db83b9af384f
dm <dmitry@udalov.online>
2025-01-05 02:27:50 +0600

    test

--- src/configuration/ostree.sh
+++ src/configuration/ostree.sh
@@ -6,7 +6,22 @@ echo "Running ostree.sh"
 mkdir -p /sysroot/ostree/repo
 ostree --repo=/sysroot/ostree/repo init --mode=archive
 mkdir -p /tmp/rootfscopy
-rsync -aA --exclude=/dev --exclude=/proc --exclude=/sys --exclude=/run --exclude=/boot --exclude=/tmp --exclude=/var/tmp --exclude=/var/lib/containers --exclude=/output / /tmp/rootfscopy/
+
+# неожиданно GNOME в /var/lib/openvpn/dev записывает устройство urandom, устройства запрещено включать в коммит
+# только файлы и сим-линки
+rsync -aA \
+  --exclude=/dev \
+  --exclude=/proc \
+  --exclude=/sys \
+  --exclude=/run \
+  --exclude=/boot \
+  --exclude=/tmp \
+  --exclude=/var/tmp \
+  --exclude=/var/lib/containers \
+  --exclude=/var/lib/openvpn/dev \
+  --exclude=/output \
+  / /tmp/rootfscopy/
+
 mkdir -p /tmp/rootfscopy/var/tmp
 ostree --repo=/sysroot/ostree/repo commit --branch=alt/atomic --subject "Initial ALT Atomic Commit" --tree=dir=/tmp/rootfscopy
 rm -rf /tmp/rootfscopy
