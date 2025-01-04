26fce86fa2693353dffd69950e7ff0d2c561a952
dm <dmitry@udalov.online>
2025-01-05 03:48:33 +0600

    test

--- src/configuration/ostree.sh
+++ src/configuration/ostree.sh
@@ -7,26 +7,21 @@ mkdir -p /sysroot/ostree/repo
 ostree --repo=/sysroot/ostree/repo init --mode=archive
 mkdir -p /tmp/rootfscopy
 
-# Неожиданно Alt linux для GNOME в /var/lib/openvpn/dev записывает устройство urandom
-# устройства запрещено включать в коммит только файлы и сим-линки
-rm -f /var/lib/openvpn/dev/urandom
-ln -s /dev/urandom /var/lib/openvpn/dev/urandom
+rsync -aA \
+  --exclude=/dev \
+  --exclude=/proc \
+  --exclude=/sys \
+  --exclude=/run \
+  --exclude=/boot \
+  --exclude=/tmp \
+  --exclude=/var/tmp \
+  --exclude=/var/lib/containers \
+  --exclude=/var/lib/openvpn/dev \
+  --exclude=/output \
+  / /tmp/rootfscopy/
 
-#rsync -aA \
-#  --exclude=/dev \
-#  --exclude=/proc \
-#  --exclude=/sys \
-#  --exclude=/run \
-#  --exclude=/boot \
-#  --exclude=/tmp \
-#  --exclude=/var/tmp \
-#  --exclude=/var/lib/containers \
-#  --exclude=/var/lib/openvpn/dev \
-#  --exclude=/output \
-#  / /tmp/rootfscopy/
-#
-#mkdir -p /tmp/rootfscopy/var/tmp
-#ostree --repo=/sysroot/ostree/repo commit --branch=alt/atomic --subject "Initial ALT Atomic Commit" --tree=dir=/tmp/rootfscopy
-#rm -rf /tmp/rootfscopy
+mkdir -p /tmp/rootfscopy/var/tmp
+ostree --repo=/sysroot/ostree/repo commit --branch=alt/atomic --subject "Initial ALT Atomic Commit" --tree=dir=/tmp/rootfscopy
+rm -rf /tmp/rootfscopy
 
 echo "End ostree.sh"
--- src/packages/DE/gnome.sh
+++ src/packages/DE/gnome.sh
@@ -4,4 +4,9 @@ echo "Installing GNOME packages"
 
 apt-get install -y gnome3-minimal
 
+# Неожиданно Alt linux для GNOME в /var/lib/openvpn/dev записывает устройство urandom
+# устройства запрещено включать в коммит, только файлы и сим-линки
+rm -f /var/lib/openvpn/dev/urandom
+ln -s /dev/urandom /var/lib/openvpn/dev/urandom
+
 echo "End installing GNOME packages"
