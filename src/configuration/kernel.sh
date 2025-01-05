b5e07d06bbbae01caef44b220377263d892290ca
dm <dmitry@udalov.online>
2025-01-05 06:54:26 +0600

    test

--- src/configuration/kernel.sh
+++ src/configuration/kernel.sh
@@ -19,8 +19,8 @@ echo "Kernel version detected: $KERNEL_VERSION"
 echo "Generating initramfs for kernel version $KERNEL_VERSION..."
 dracut --force \
        --kver "$KERNEL_VERSION" \
-       --add "qemu ostree virtiofs btrfs" \
-       --add-drivers "virtio_blk virtio_pci virtio_net virtio_gpu drm i915 amdgpu" \
+       --add "qemu ostree virtiofs btrfs udev network base" \
+       --add-drivers "virtio_blk virtio_pci virtio_net virtio_gpu drm i915 amdgpu snd_hda_intel snd_hda_codec snd_hda_core snd_pcm snd_timer soundcore joydev evdev btrfs overlay fuse virtio_rng virtio_console virtio_balloon net_failover virtio_dma_buf" \
        "${BOOT_DIR}/initramfs-${KERNEL_VERSION}.img"
 
 # Копируем vmlinuz и initramfs
