8a9a16f24ce639eca6d59fb26a62aa3d9d701818
dm <dmitry@udalov.online>
2025-01-05 19:20:04 +0600

    test

--- src/configuration/kernel.sh
+++ src/configuration/kernel.sh
@@ -17,12 +17,56 @@ fi
 
 echo "Kernel version detected: $KERNEL_VERSION"
 echo "Generating initramfs for kernel version $KERNEL_VERSION..."
+
+# dracut: создаем initramfs с нужными модулями и функциями
 dracut --force \
        --kver "$KERNEL_VERSION" \
        --add "qemu ostree virtiofs btrfs base" \
-       --add-drivers "virtio_blk virtio_pci virtio_net virtio_gpu drm i915 amdgpu snd_hda_intel snd_hda_codec snd_hda_core snd_pcm snd_timer soundcore joydev evdev btrfs overlay fuse virtio_rng virtio_console virtio_balloon net_failover virtio_dma_buf" \
+       --add-drivers "
+           ext4
+           ahci
+           ahci_platform
+           sd_mod
+           evdev
+           virtio_blk
+           virtio_pci
+           virtio_net
+           virtio-gpu
+           virtio_input
+           virtio_scsi
+           virtio_console
+           virtio_blk
+           virtio-rng
+           virtio_net
+           virtio_pci
+           virtio-mmio
+           drm/virtio
+           virtio_input
+           i915
+           amdgpu
+           snd_hda_intel
+           snd_hda_codec
+           snd_hda_core
+           snd_pcm
+           snd_timer
+           soundcore
+           joydev
+           evdev
+           overlay
+           fuse
+           net_failover
+           crc32_generic
+           ata_piix
+           drivers/hid
+           drivers/pci
+           drivers/mmc
+           drivers/usb/host
+           drivers/usb/storage
+           drivers/nvmem
+           drivers/nvme
+           drivers/video/fbdev
+       " \
        "${BOOT_DIR}/initramfs-${KERNEL_VERSION}.img"
-
 
 # Копируем vmlinuz и initramfs
 echo "Copying vmlinuz and initramfs..."
