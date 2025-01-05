73cd84b2b4ac44264511347434ef573f62e1e258
dm <dmitry@udalov.online>
2025-01-05 20:25:48 +0600

    test

--- src/main.sh
+++ src/main.sh
@@ -5,20 +5,21 @@ set -e
 
 echo "Running main.sh..."
 
-# Пакеты
-./packages/apt_prepare.sh
-./packages/base.sh           # базовые пакеты для работы системы
-./packages/DE/gnome.sh       # почему бы и нет
-./packages/apt_ending.sh
-
-# Настройка
-./configuration/branding.sh
-./configuration/settings.sh
-./configuration/kernel.sh
-./make/zstd.sh
-./make/cargo.sh
-./make/bootupd.sh
-./make/bootc.sh
-./configuration/ostree.sh
+cat "/src/configuration/source/initrd.mk.oem"
+## Пакеты
+#./packages/apt_prepare.sh
+#./packages/base.sh           # базовые пакеты для работы системы
+#./packages/DE/gnome.sh       # почему бы и нет
+#./packages/apt_ending.sh
+#
+## Настройка
+#./configuration/branding.sh
+#./configuration/settings.sh
+#./configuration/kernel.sh
+#./make/zstd.sh
+#./make/cargo.sh
+#./make/bootupd.sh
+#./make/bootc.sh
+#./configuration/ostree.sh
 
 echo "All scripts executed successfully."
