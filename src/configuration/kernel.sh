cdcc2721c28a1627bf43afd19877d02d8a08d137
dm <dmitry@udalov.online>
2025-01-05 20:19:20 +0600

    test

--- src/configuration/kernel.sh
+++ src/configuration/kernel.sh
@@ -6,8 +6,8 @@ echo "Running kernel_setup.sh..."
 # Определяем пути
 KERNEL_DIR="/usr/lib/modules"
 BOOT_DIR="/boot"
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"  # Абсолютный путь к директории скрипта
-CONFIG_FILE="$SCRIPT_DIR/../source/initrd.mk.oem"  # Абсолютный путь к initrd.mk.oem
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CONFIG_FILE="$SCRIPT_DIR/source/initrd.mk.oem"  # Абсолютный путь к initrd.mk.oem
 
 # Определяем версии ядра
 kver=$(rpm -qa 'kernel-image*' --qf '%{version}-%{name}-%{release}\n' | sed 's/kernel-image-//')
--- src/main.sh
+++ src/main.sh
@@ -5,25 +5,20 @@ set -e
 
 echo "Running main.sh..."
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"  # Абсолютный путь к директории скрипта
-CONFIG_FILE="$SCRIPT_DIR/source/initrd.mk.oem"  # Абсолютный путь к initrd.mk.oem
-
-echo "$CONFIG_FILE"
-cat "$CONFIG_FILE"
 # Пакеты
-#./packages/apt_prepare.sh
-#./packages/base.sh           # базовые пакеты для работы системы
-#./packages/DE/gnome.sh       # почему бы и нет
-#./packages/apt_ending.sh
-#
-## Настройка
-#./configuration/branding.sh
-#./configuration/settings.sh
-#./configuration/kernel.sh
-#./make/zstd.sh
-#./make/cargo.sh
-#./make/bootupd.sh
-#./make/bootc.sh
-#./configuration/ostree.sh
+./packages/apt_prepare.sh
+./packages/base.sh           # базовые пакеты для работы системы
+./packages/DE/gnome.sh       # почему бы и нет
+./packages/apt_ending.sh
+
+# Настройка
+./configuration/branding.sh
+./configuration/settings.sh
+./configuration/kernel.sh
+./make/zstd.sh
+./make/cargo.sh
+./make/bootupd.sh
+./make/bootc.sh
+./configuration/ostree.sh
 
 echo "All scripts executed successfully."
