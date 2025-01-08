3f538925e708101c86dda2a4b2028694fddd542f
dm <dmitry@udalov.online>
2025-01-08 16:43:36 +0600

    test

--- src/packages/base.sh
+++ src/packages/base.sh
@@ -61,8 +61,6 @@ apt-get install -y \
     kernel-image-6.12 \
     kernel-modules-drm-6.12 \
     kernel-headers-6.12 \
-    kernel-modules-drm-nouveau-6.12 \
-    kernel-modules-drm-radeon-6.12 \
     rsync \
     plymouth \
     mesa-dri-drivers \
