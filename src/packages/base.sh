ef0e3669b82f70a30079b906fb0b80715c170746
dm <dmitry@udalov.online>
2025-01-08 16:16:04 +0600

    test

--- src/packages/base.sh
+++ src/packages/base.sh
@@ -60,14 +60,11 @@ apt-get install -y \
     dracut \
     kernel-image-6.12 \
     kernel-modules-drm-6.12 \
-    kernel-modules-drm-nouveau-6.12 \
-    kernel-modules-drm-radeon-6.12 \
     kernel-headers-6.12 \
     rsync \
     plymouth \
     mesa-dri-drivers \
     glxinfo \
-    inxi \
     ntp
 
 echo "End installing base packages"
