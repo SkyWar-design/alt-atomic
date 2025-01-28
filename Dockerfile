1bd14ba7a7787c4d9048357bfb0aeb219dd9dfcf
dm <dmitry@udalov.online>
2025-01-28 21:26:51 +0600

    test another docker build

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM registry.altlinux.org/sisyphus/base:latest
 
-ARG BUILD_TYPE="default"
-ENV BUILD_TYPE=$BUILD_TYPE
-
-RUN echo "Current BUILD_TYPE: $BUILD_TYPE"
 # Копируем скрипты
 COPY src /src
 
@@ -11,14 +7,19 @@ COPY src /src
 ARG PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/lib/pkgconfig"
 ARG PATH="/root/.cargo/bin:${PATH}"
 
+# Определяем тип сборки
+ARG BUILD_TYPE="default"
+ENV BUILD_TYPE=$BUILD_TYPE
+
 WORKDIR /src
 # Делаем один RUN запуск, потому что увеличние их числа добавляет ненужные слои и увеличивает обьем образа
-#RUN ./main.sh
+RUN ./main.sh
 
 WORKDIR /
 
 # Помечаем образ как bootc совместимый
 LABEL containers.bootc=1
+
 # Оптимизация для Buildx
 ARG BUILDKIT_INLINE_CACHE=1
 
--- src/main.sh
+++ src/main.sh
@@ -6,26 +6,31 @@ set -e
 
 echo "Running main.sh..."
 
-# Базовые пакеты для работы системы
-./packages/apt_prepare.sh
-./packages/base.sh
-./packages/DE/GNOME/gnome.sh
-./packages/apt_ending.sh
+# Условие для NVIDIA сборки
+if [ "$BUILD_TYPE" = "nvidia" ]; then
+    echo "Installing NVIDIA components..."
+fi
 
-# Настройка
-./configuration/branding.sh
-./configuration/settings.sh
-./configuration/user.sh
-./configuration/kernel.sh
-
-# Сборка программ
-./make/zstd.sh
-./make/cargo.sh
-./make/bootupd.sh
-./make/bootc.sh
-./make/brew.sh
-./make/zsh-plugins.sh
-./make/atomic-actions.sh
-./configuration/clear.sh
+## Базовые пакеты для работы системы
+#./packages/apt_prepare.sh
+#./packages/base.sh
+#./packages/DE/GNOME/gnome.sh
+#./packages/apt_ending.sh
+#
+## Настройка
+#./configuration/branding.sh
+#./configuration/settings.sh
+#./configuration/user.sh
+#./configuration/kernel.sh
+#
+## Сборка программ
+#./make/zstd.sh
+#./make/cargo.sh
+#./make/bootupd.sh
+#./make/bootc.sh
+#./make/brew.sh
+#./make/zsh-plugins.sh
+#./make/atomic-actions.sh
+#./configuration/clear.sh
 
 echo "All scripts executed successfully."
